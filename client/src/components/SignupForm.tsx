55d639ead27b1e9207620fc5c720303a38d3fd24
Thida612 <angelic722@gmail.com>
2025-02-07 09:32:16 -0600

    Add user signup mutation in SignupForm component

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-import type { User } from '../models/User';
+
 
 // biome-ignore lint/correctness/noEmptyPattern: <explanation>
 const SignupForm = ({}: { handleModalClose: () => void }) => {
